d6406020c9ab7e5a6438e39cf56ab3ba5a44c111
Dariia Butorina <dariia.butorina@icloud.com>
2026-03-19 18:04:30 +0300

    Fixed if statements

--- app/main.py
+++ app/main.py
@@ -11,18 +11,16 @@ def create_person_list(people: list) -> list:
     person_list = [Person(human["name"], human["age"]) for human in people]
 
     for human in people:
-        if "wife" in human:
-            if human["wife"] is not None:
-                Person.people[human["name"]].wife = (
-                    Person.people)[human["wife"]]
-                Person.people[human["wife"]].husband = (
-                    Person.people)[human["name"]]
+        if human.get("wife") is not None:
+            Person.people[human["name"]].wife = (
+                Person.people)[human["wife"]]
+            Person.people[human["wife"]].husband = (
+                Person.people)[human["name"]]
 
-        if "husband" in human:
-            if human["husband"] is not None:
-                Person.people[human["name"]].husband = (
-                    Person.people)[human["husband"]]
-                Person.people[human["husband"]].wife = (
-                    Person.people)[human["name"]]
+        if human.get("husband") is not None:
+            Person.people[human["name"]].husband = (
+                Person.people)[human["husband"]]
+            Person.people[human["husband"]].wife = (
+                Person.people)[human["name"]]
 
     return person_list
